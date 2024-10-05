55ca20604a940a8ae3c612b0f0718910c7711ca9
Shashwat Suri <shashwatsuri98@gmail.com>
2024-10-05 12:12:47 -0700

    Adding synthetic scene and plugging it in the sh file

--- run_scripts/run_all_synthetic.sh
+++ run_scripts/run_all_synthetic.sh
@@ -4,6 +4,7 @@ export LAMBDA_SPRING=0.0
 export ISOMETRY=0.3
 export LAMBDA_VELOCITY=0.0
 export LAMBDA_MOMENTUM=0.03
+export DATA_DIR="/scratch-ssd/Datasets/deformgs/synthetic"
 
 export SCENE_1="scene_1"
 export SCENE_2="scene_2"
@@ -13,14 +14,19 @@ export SCENE_5="scene_5"
 export SCENE_6="scene_6"
 
 port=6058 
-for SCENE in $SCENE_1 $SCENE_2 $SCENE_3 $SCENE_4 $SCENE_5 $SCENE_6;
-do
-    for isometry in $ISOMETRY;
-    do 
-        python3 train.py -s "data/synthetic/${SCENE}" --port $port --expname "synthetic/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --lambda_w 2000 \
+# for SCENE in $SCENE_1 $SCENE_2 $SCENE_3 $SCENE_4 $SCENE_5 $SCENE_6;
+# do
+#     for isometry in $ISOMETRY;
+#     do 
+#         python3 train.py -s "data/synthetic/${SCENE}" --port $port --expname "synthetic/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --lambda_w 2000 \
+#         --lambda_rigidity $RIGIDITY_LAMBDA --lambda_spring $LAMBDA_SPRING  --lambda_momentum $LAMBDA_MOMENTUM --lambda_velocity $LAMBDA_VELOCITY --view_skip 1 --time_skip 1 \
+#        --checkpoint_iterations 1000 19000 20000  \
+#        --k_nearest 20 --lambda_isometric $isometry --reg_iter 11000 --staticfying_from 10000 --use_wandb --wandb_project "synthetic_${SCENE}"  --wandb_name "init" 
+#         port=$((port+1))
+#     done
+# done
+
+python3 train.py -s "{$DATA_DIR}/${SCENE_3}" --port $port --expname "synthetic/${SCENE}" --configs arguments/mdnerf-dataset/cube.py --lambda_w 2000 \
         --lambda_rigidity $RIGIDITY_LAMBDA --lambda_spring $LAMBDA_SPRING  --lambda_momentum $LAMBDA_MOMENTUM --lambda_velocity $LAMBDA_VELOCITY --view_skip 1 --time_skip 1 \
        --checkpoint_iterations 1000 19000 20000  \
        --k_nearest 20 --lambda_isometric $isometry --reg_iter 11000 --staticfying_from 10000 --use_wandb --wandb_project "synthetic_${SCENE}"  --wandb_name "init" 
-        port=$((port+1))
-    done
-done
