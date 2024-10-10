42e94242bb0d034b751c38f89c9cd4cef7487149
Shashwat Suri <shashwatsuri98@gmail.com>
2024-10-09 17:58:11 -0700

    changes to config

--- scene/multipleview_dataset.py
+++ scene/multipleview_dataset.py
@@ -28,10 +28,10 @@ class multipleview_dataset(Dataset):
         self.FovY = focal2fov(self.focal[0], height)
         self.FovX = focal2fov(self.focal[0], width)
         self.transform = Tr.ToTensor()
-        self.image_paths, self.image_poses, self.image_times= self.load_images_path(cam_folder, cam_extrinsics,cam_intrinsics,split)
+        self.image_paths, self.image_poses, self.image_times, self.view_ids, self.time_ids= self.load_images_path(cam_folder, cam_extrinsics,cam_intrinsics,split)
         if split=="test":
             cam_extrinsics = {key:cam_extrinsics[key]}
-            self.image_paths, self.image_poses, self.image_times= self.load_images_path(cam_folder, cam_extrinsics,cam_intrinsics,split)
+            self.image_paths, self.image_poses, self.image_times, self.view_ids, self.time_ids= self.load_images_path(cam_folder, cam_extrinsics,cam_intrinsics,split)
 
             # self.video_cam_infos=self.get_video_cam_infos(cam_folder)
             cam_infos = []
@@ -66,7 +66,7 @@ class multipleview_dataset(Dataset):
                 time = self.image_times[i]
                 cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
                                     image_path=image_path, image_name=image_name, width=width, height=height,
-                                    time = time, mask=None) # default by monocular settings.
+                                    time = time, mask=None, time_id=None,view_id=None) # default by monocular settings.
                 cam_infos.append(cam_info)
             self.video_cam_infos = cam_infos
 
@@ -78,26 +78,31 @@ class multipleview_dataset(Dataset):
         image_paths=[]
         image_poses=[]
         image_times=[]
+        view_ids=[]
+        time_ids=[]
         for idx, key in enumerate(cam_extrinsics):
             extr = cam_extrinsics[key]
             R = np.transpose(qvec2rotmat(extr.qvec))
             T = np.array(extr.tvec)
 
-            number = os.path.basename(extr.name)[5:-4]
-            images_folder=os.path.join(cam_folder,"cam"+number.zfill(2))
+            view_id = os.path.basename(extr.name)[5:-4]
+            images_folder=os.path.join(cam_folder,"cam"+view_id.zfill(2))
 
             image_range=range(image_length)
             # if split=="test":
             #     image_range = [image_range[0],image_range[int(image_length/3)],image_range[int(image_length*2/3)]]
 
             for i in image_range:    
-                num=i+1
-                image_path=os.path.join(images_folder,"frame_"+str(num).zfill(5)+".png")
+                time_id=i+1
+                image_path=os.path.join(images_folder,"frame_"+str(time_id).zfill(5)+".png")
                 image_paths.append(image_path)
                 image_poses.append((R,T))
                 image_times.append(float(i/image_length))
+                view_ids.append(view_id)
+                time_ids.append(time_id)
 
-        return image_paths, image_poses,image_times
+
+        return image_paths, image_poses,image_times, view_ids,time_ids
     
     def get_video_cam_infos(self,datadir):
         poses_arr = np.load(os.path.join(datadir, "poses_bounds_multipleview.npy"))
